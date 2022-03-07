436dca58709ed11f3a9768e8c94d65a19aca72eb
Bora Kaya <bora.587@hotmail.com>
2022-03-08 00:27:53 +0300

    Add project files.

---
+++ ArrayList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList dizi = new ArrayList();
+
+            dizi.Add("Kedi");
+            dizi.Add(99);
+            dizi.Add(true);
+
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(dizi);
+            }
+        }
+    }
+}
